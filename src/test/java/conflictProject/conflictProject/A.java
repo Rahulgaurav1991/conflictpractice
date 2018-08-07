abf6cab14c7f7e5e522ac6cfbf95951213cc4895
rahulgaurav <rahulgaurav1991@gmail.com>
2018-08-07 13:22:29 +0530

    Rahul conflictcode

---
+++ src/test/java/conflictProject/conflictProject/A.java
@@ -0,0 +1,35 @@
+package conflictProject.conflictProject;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class A {
+	
+	WebDriver driver;
+	
+	@Test
+	public void mouseActions() {
+		Actions act = new Actions(driver);
+		act.moveToElement(driver.findElement(By.xpath("//button[.='Automation Tools']"))).perform();
+		List<WebElement> all = driver.findElements(By.xpath("//button[.='Automation Tools']/..//a"));
+		for (WebElement name : all) {
+			String text = name.getText();
+			System.out.println(text);
+			System.out.println(name.isEnabled());
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			if (text.equalsIgnoreCase("Appium")) {
+				name.click();
+				break;
+
+			}
+
+		}
+	}
+
+}
